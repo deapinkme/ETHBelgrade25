98a839c1907d0d9668113ce41c7b18fa73bbcc59
deapinkme <deapinkme@gmail.com>
2025-06-05 04:38:52 +0200

    current

---
+++ packages/nextjs/environments.ts
@@ -0,0 +1,9 @@
+const PROXY_ENV = "devnet";
+const SOLANA_URL = "https://api.devnet.solana.com";
+const NEON_CORE_API_RPC_URL = "https://devnet.neonevm.org";
+
+export {
+  PROXY_ENV,
+  SOLANA_URL,
+  NEON_CORE_API_RPC_URL,
+};
